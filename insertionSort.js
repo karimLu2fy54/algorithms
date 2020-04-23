b9f666f3dd3c5f3f98a75c8f9876ace651ff0798
Lu2fy54 <kkhelil54@gmail.com>
2020-04-23 14:33:15 +0200

    tri par insertion fonctionnel

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,33 @@
 function insertionSort(array) {
+  
 
-  var longueur = array.length;
+var longueur = array.length;
 
-  for(var i=1;i<longueur;i++){
+for(var i=1;i<longueur;i++){
 
-    for(var j=i;j>0;j--){
-        if(array[i]>)
-    }
+  var nombreAranger = array[i];
+  var positionNombreARanger = i;
 
+  var j=positionNombreARanger;
+
+  while(j>=0 & nombreAranger < tab[j-1]){
+    array[j] = array[j-1];
+    j=j-1;
+    console.log(array);
   }
+  array[j] = nombreAranger;
 
+}
+
+
+  console.log('resultat final');
+  console.log(array);
+  return array;
+  
+
+  
+
+  
 }
 
 insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
