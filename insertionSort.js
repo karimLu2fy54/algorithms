9a6d059f0cc2acdd10072cce79c1877004ca593e
Lu2fy54 <kkhelil54@gmail.com>
2020-05-25 10:45:30 +0200

    ajout de la fonction estTrie()

--- insertionSort.js
+++ insertionSort.js
@@ -31,3 +31,29 @@ for(var i=1;i<longueur;i++){
 }
 
 insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+
+
+function estTrie(array){
+
+  var tableauTrie = true;
+
+  for(var i=0;i<array.length-1;i++){
+      if(array[i] > array[i+1]){
+        tableauTrie = false;
+        break;
+
+      }
+
+  }
+
+  console.log(tableauTrie);
+  return tableauTrie;
+  
+
+}
+
+//return true
+estTrie(insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
+
+//return false
+estTrie([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
